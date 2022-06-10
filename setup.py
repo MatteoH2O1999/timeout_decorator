f3d8330ff84ba1c08f6523034de1866606cf8485
MatteoH2O1999 <matteo.dellacqua2@studenti.unimi.it>
2022-06-11 00:33:56 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='timeout_decorator',
+    version='1.0.0',
+    packages=[''],
+    url='',
+    license='',
+    author='MatteoH2O1999',
+    author_email='dellacqua.matteo99@gmail.com',
+    description=''
+)
---
+++ src/timeout_decorator/timeout_decorator.py
@@ -0,0 +1,2 @@
+def timeout(func, timeout=None, exception_to_raise=TimeoutError):
+    pass
